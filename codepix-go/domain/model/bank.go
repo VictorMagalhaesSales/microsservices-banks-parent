4ae37dfbb449e1391a9cdbea392c38bc9ea03668
victormagalhaessales <victormagalhaessales@gmail.com>
2021-03-25 17:01:11 -0300

    feat(master) adding all models

--- codepix-go/domain/model/bank.go
+++ codepix-go/domain/model/bank.go
@@ -1,25 +1,41 @@
 package model
 
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
 type Bank struct {
-	Base
-	Code 	string `json: "Code"`
-	Name 	string `json: "Name"`
+	Base     `valid:"required"`
+	Code     string     `json:"code" gorm:"type:varchar(20)" valid:"notnull"`
+	Name     string     `json:"name" gorm:"type:varchar(255)" valid:"notnull"`
+	Accounts []*Account `gorm:"ForeignKey:BankID" valid:"-"`
 }
 
 /*
 * Anexando a fução à struct Bank
-*/
+ */
 func (bank *Bank) isValid() error {
+	_, err := govalidator.ValidateStruct(bank)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-/* 
+/*
 * retornará um Bank ou error
-*/
+ */
 func NewBank(code string, name string) (*Bank, error) {
 	bank := Bank{
 		Code: code,
-		Name: name
+		Name: name,
 	}
 
 	bank.ID = uuid.NewV4().String()
--- codepix-go/domain/model/base.go
+++ codepix-go/domain/model/base.go
@@ -1,12 +1,17 @@
 package model
 
 import (
-	uuid "github.com/satori/go.uuid"
 	"time"
+
+	"github.com/asaskevich/govalidator"
 )
 
-type Base{
-	ID string `json: "id"`
-	CreatedAt time.Time `json: "CreatedAt"`
-	UpdatedAt time.Time `json: "UpdatedAt"`
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type Base struct {
+	ID        string    `json:"id" gorm:"type:uuid;primary_key" valid:"uuid"`
+	CreatedAt time.Time `json:"created_at" valid:"-"`
+	UpdatedAt time.Time `json:"updated_at" valid:"-"`
 }
