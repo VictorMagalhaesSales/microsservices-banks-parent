28682e08312b8c9f305bde1a3c2b492ccdc12cd1
victormagalhaessales <victormagalhaessales@gmail.com>
2021-03-30 16:57:48 -0300

    Configuring TypeOrmModule in AppModule

--- bank-api-nestjs/src/app.module.ts
+++ bank-api-nestjs/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConsoleModule } from 'nestjs-console';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankAccount } from './models/bank-account.model';
+import { PixKey } from './models/pix-key.model';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ConsoleModule,
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      // Entidades que poderão ser utilizadas pela aplicação
+      entities: [BankAccount, PixKey]
+    }),
+    TypeOrmModule.forFeature([BankAccount, PixKey]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
