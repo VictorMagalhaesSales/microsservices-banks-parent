b3abe44344ade573c2046308ecf0a47325961871
victormagalhaessales <victormagalhaessales@gmail.com>
2021-03-30 17:21:42 -0300

    Create controllers base bank and pix-key

---
+++ bank-api-nestjs/src/controllers/bank-account/bank-account.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BankAccount } from 'src/models/bank-account.model';
+import { Repository } from 'typeorm';
+
+@Controller('bank-account')
+export class BankAccountController {
+
+    constructor(
+        @InjectRepository(BankAccount)
+        private bankAccountRepo: Repository<BankAccount>
+    ) {}
+
+    @Get()
+    private list() {
+        return this.bankAccountRepo.find();
+    }
+
+}
