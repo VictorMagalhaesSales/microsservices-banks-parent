461f938ada946feb39cdb5957e6b8c100f0502ea
victormagalhaessales <victormagalhaessales@gmail.com>
2021-04-01 10:41:51 -0300

    Implementing creation of pix-key

--- api-bank-nestjs/README.md
+++ api-bank-nestjs/README.md
@@ -1,8 +1,9 @@
 # api-bank-nestjs
 Microservice feito em NestJS com o objetivo de controlar os **bancos** que utilizaremos durante o projeto. 
-Foi utilizado o **TypeORM** como ORM e o **nestjs-console** junto com **commander** para fazer a carga de dados inicial([console.ts](https://github.com/VictorMagalhaesSales/microsservices-banks-parent/blob/master/api-bank-nestjs/src/console.ts) e [fixtures.command.ts](https://github.com/VictorMagalhaesSales/microsservices-banks-parent/blob/master/api-bank-nestjs/src/utils/fixtures/fixtures.command.ts));
+Foi utilizado o **TypeORM** como ORM e o **nestjs-console** junto com **commander** para fazer uma carga inicial de dados([console.ts](https://github.com/VictorMagalhaesSales/microsservices-banks-parent/blob/master/api-bank-nestjs/src/console.ts) e [fixtures.command.ts](https://github.com/VictorMagalhaesSales/microsservices-banks-parent/blob/master/api-bank-nestjs/src/utils/fixtures/fixtures.command.ts));
 
 - **@nestjs/microservices**: habilita o projeto como client para consumir outros serviços. Basta adicionar `ClientsModule.register([clientGrpcConfig, clientConfig2])`ao seu módulo;
+- **class-validator**: nos permite criar validações(string, tipo e etc) em propriedade dos nossos models;
 
 ## Running the app
 ```bash
--- api-bank-nestjs/src/controllers/pix-key.controller.ts
+++ api-bank-nestjs/src/controllers/pix-key.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, ValidationPipe } from '@nestjs/common';
 import { PixKey } from '../models/pix-key.model';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PixKeyDto } from 'src/models/dto/pix-key.dto';
+import { BankAccount } from 'src/models/bank-account.model';
 
 @Controller('bank-accounts/:bankAccountId/pix-keys')
 export class PixKeyController {
 
     constructor(
-        @InjectRepository(PixKey)
-        private pixKeyRepo: Repository<PixKey>
+      @InjectRepository(PixKey)
+      private pixKeyRepo: Repository<PixKey>,
+      @InjectRepository(BankAccount)
+      private bankAccountRepo: Repository<BankAccount>
     ) {}
 
     @Get()
@@ -24,5 +28,21 @@ export class PixKeyController {
           created_at: 'DESC',
         },
       });
+    }
+
+    @Post()
+    store(
+      @Param('bankAccountId', new ParseUUIDPipe({ version: '4' }))
+      bankAccountId: string,
+      @Body(new ValidationPipe({errorHttpStatusCode: 422}))
+      body: PixKeyDto
+    ) {
+      this.bankAccountRepo.findOneOrFail(bankAccountId);
+
+      const pixKey = this.pixKeyRepo.create({
+        bank_account_id: bankAccountId,
+        ...body,
+      });
+      return this.pixKeyRepo.save(pixKey);
     }
 }
