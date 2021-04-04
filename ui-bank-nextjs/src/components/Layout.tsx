971c585a9f4da997d53bd1f956d2d9e7cfd48c4c
victormagalhaessales <victormagalhaessales@gmail.com>
2021-04-04 13:11:01 -0300

    Creating MainContent component

--- ui-bank-nextjs/src/components/Layout.tsx
+++ ui-bank-nextjs/src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import Footer from "./Footer";
+import MainContent from "./MainContent";
 import Navbar from "./Navbar";
 
 export default function Layout() {
     return (
         <div>
             <Navbar/>
+            <MainContent>
+                <h1>Contas bancárias</h1>
+            </MainContent>
             <Footer/>
         </div>
     );
